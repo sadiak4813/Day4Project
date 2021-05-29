ac998fb7be95fc29fed9ac7fc581d526e4c80b7b
sadiak4813 <sadiak4813@gmail.com>
2021-05-29 10:41:52 -0500

    shared on github

---
+++ src/test/java/BranchingPractice.java
@@ -0,0 +1,11 @@
+public class BranchingPractice {
+
+    public static void main(String[] args) {
+        // created new project
+        // enabled version control
+        // added /gitignore with .idea , target folder and .iml file
+        //shared project on gitbhub
+
+    }
+
+}
